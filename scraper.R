fbd29972d5f8f906613f7b1b881a31d529024763
Marco C. Ziegler <Marco_Ziegler@GMX.de>
2020-07-02 23:29:12 +0200

    finish

--- scraper.R
+++ scraper.R
@@ -14,60 +14,62 @@
 # chose a url an read its content
 # this is our startpage: 
 # "https://www.thomann.de/de/lp-modelle.html?pg=1&ls=25"
-# first we prep the url a bit to iterate through all the guitars 
 
-url <- "https://www.thomann.de/de/lp-modelle.html?"
+# first we prep the url a bit to iterate through all the guitars 
+  url <- "https://www.thomann.de/de/lp-modelle.html?"
 
 # results per page 
-rpp <- 25
+  rpp <- 25
 
 # check how many entries we get from our start-page
-parsed_page <- xml2::read_html(url)
-results_count <-  as.numeric(html_text(html_nodes(parsed_page,".result-count")))
+  parsed_page <- xml2::read_html(url)
+  results_count <-  as.numeric(html_text(html_nodes(parsed_page,".result-count")))
+
+#define how many pages we have to search
+  surf <- ceiling(results_count / rpp)
 
 # setup pages to browsr through
-list_of_pages <- str_c(url, "pg=", 1:rpp, "&ls=25")
+  list_of_pages <- str_c(url, "pg=", 1:surf, "&ls=25")
 
-manufac_list = array()
-model_list = array()
-guitar_list = data.frame()
+#a dataframe to bind them all! 
+  masterframe <- data.frame()
 
-for (page in list_of_pages) {
+# iterate through the pages and scrape data
+    for (page in list_of_pages) {
       
       #take the information u need
       print(c("scraping page:",page))
-      
+  
       #scraping the manufacturers
-      manufacturers <-html_text(html_nodes(xml2::read_html(page),".manufacturer"))
-      manufacturers <-manufacturers[3:(rpp+2)]
-      manufac_list <- c(manufac_list,manufacturers)
-      
+      manufacturers <-html_text(html_nodes(xml2::read_html(page),"#defaultResultPage .manufacturer"))
+      print(length(manufacturers))
+     
       #scraping the models
       models <- html_text(html_nodes(xml2::read_html(page),".model"))
-      model_list <- c(model_list,models)
+      #print(models)
+      print(length(models))
       
-      tmp <- as.data.frame(list(na.omit(manufacturers),na.omit(models)))
-      colnames(tmp) <-c("Manufacturer","Model")
-      guitar_list <-rbind(guitar_list,tmp)
+      #scraping prices
+      price <- html_text(html_nodes(xml2::read_html(page),".article-basketlink"))
+      print(length(price))
       
-}
+      masterframe <-rbind(masterframe,data.frame(cbind(manufacturers,models,price)))
+                 
+    }
 
+# replace annoying characters
+  masterframe$price <- as.numeric(gsub("[€.]","",masterframe$price))
 
-#manufac_list <- unique(na.omit(manufac_list))
+# find the average price for a guitar by manufacturer
+  table <-aggregate(masterframe$price ,by = list(masterframe$manufacturers),mean)
 
-#clean data
+  names(table) <- c("manufacturer","average.price")
 
+  table <-arrange(table, average.price)
 
+# make a little visualisation
 
-
-
-
-
-
-
-
-
-
-
-
-
+  plot <- ggplot(data = table, aes(x = manufacturer, y = average.price)) +
+    geom_bar(stat = "identity") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 90)) 
